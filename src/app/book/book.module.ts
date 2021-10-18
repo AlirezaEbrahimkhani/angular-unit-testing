c87ccbe6bcd108500fce678e27544c7c761ba922
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-18 23:21:45 +0330

    [Feat] create add book component

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -5,9 +5,10 @@ import { BookListComponent } from './components/book-list/book-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { BookRoutingComponent } from './book-routing.component';
 import { BookRoutingModule } from './book.routing';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
 @NgModule({
-  declarations: [ReplaceUnderlinePipe, BookListComponent, BookRoutingComponent],
+  declarations: [ReplaceUnderlinePipe, BookListComponent, BookRoutingComponent, AddBookComponent],
   imports: [CommonModule, MatCardModule, BookRoutingModule],
   exports: [BookListComponent],
 })
