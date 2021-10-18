7c0bab680356adce04e8999cacf969832554b8c0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-19 00:18:34 +0330

    [Test] testing the directive

--- src/app/book/shared/directives/hover-focus.directive.ts
+++ src/app/book/shared/directives/hover-focus.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHoverFocus]'
+  selector: '[appHoverFocus]',
 })
 export class HoverFocusDirective {
+  @HostBinding('style.background-color') backgroundColor: string = "";
 
-  constructor() { }
+  @HostListener('mouseover') onHover() {
+    this.backgroundColor = 'blue';
+  }
 
+  @HostListener('mouseout') onLeave() {
+    this.backgroundColor = 'inherit';
+  }
 }
