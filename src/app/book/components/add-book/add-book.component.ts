b39a334c3482fa3a8f3a08549c263693bd75cdf2
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-18 23:53:52 +0330

    [Test] testing input and output of the component

--- src/app/book/components/add-book/add-book.component.ts
+++ src/app/book/components/add-book/add-book.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+  styleUrls: ['./add-book.component.scss'],
 })
-export class AddBookComponent implements OnInit {
+export class AddBookComponent {
+  @Output() loggedIn = new EventEmitter<any>();
+  @Input() enabled = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addBook(email: any, password: any) {
+    console.log(`Login ${email} ${password}`);
+    if (email && password) {
+      console.log(`Emitting`);
+      this.loggedIn.emit(new Book(email, password));
+    }
   }
+}
 
+export class Book {
+  constructor(public title: string, public password: string) {}
 }
