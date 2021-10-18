4c670c973114792dc64bf969ef4af0bc394cc0d2
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-18 23:54:38 +0330

    [Feat] create hover focus directive

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -6,9 +6,10 @@ import { MatCardModule } from '@angular/material/card';
 import { BookRoutingComponent } from './book-routing.component';
 import { BookRoutingModule } from './book.routing';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { HoverFocusDirective } from './shared/directives/hover-focus.directive';
 
 @NgModule({
-  declarations: [ReplaceUnderlinePipe, BookListComponent, BookRoutingComponent, AddBookComponent],
+  declarations: [ReplaceUnderlinePipe, BookListComponent, BookRoutingComponent, AddBookComponent, HoverFocusDirective],
   imports: [CommonModule, MatCardModule, BookRoutingModule],
   exports: [BookListComponent],
 })
